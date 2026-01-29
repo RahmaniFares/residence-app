1650fd3727c88d1c3718803a0e5092ba2ee22587
RAHMANI Fares <fares.rahmani@etudiant-isi.utm.tn>
2026-01-29 23:13:22 +0100

    mobile adjust

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
   AppName = 'Residom';
+  showMobileMenu = false;
+
+  toggleMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
 }
